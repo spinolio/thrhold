8290889e4b0204dd7f5074c4ad6253eeaa0864db
Steve B <94152075+spinolio@users.noreply.github.com>
2023-07-17 21:37:40 -0500

    Create main.lua
    Initial checkin of throttle hold indicator widget.

---
+++ ThrHold/main.lua
@@ -0,0 +1,59 @@
+-- A simple throttle hold indicator
+-- Steve Bernard
+-- Date: 2023
+-- ver: 0.1
+
+local options = {
+  { "TextColor", COLOR, YELLOW },
+  { "Source", SOURCE, 0},
+  { "Invert", BOOL, 0},
+}
+
+local function log(s)
+  return;
+  --print("thrhold: " .. s)
+end
+
+local function create(zone, options)
+  local wgt = { zone = zone, options = options }
+  return wgt
+end
+
+local function update(wgt, options)
+  if (wgt == nil) then return end
+  wgt.options = options
+  --log("src:" .. options.Source)
+end
+
+local function background(wgt)
+  return
+end
+
+------------------------------------------------------------
+
+
+local function refresh(wgt, event, touchState)
+  if (wgt == nil)               then log("refresh(nil)")                   return end
+  if (wgt.options == nil)       then log("refresh(wgt.options=nil)")       return end
+
+    local font_size = MIDSIZE
+    local hold_str
+    textColor = wgt.options.TextColor
+    local val = getValue(wgt.options.Source)
+    if wgt.options.Invert == 1 then
+        val = val * -1
+    end
+    --log("src:val" .. val)
+    if val < 0 then
+        hold_str = "ACTV"
+    elseif val > 0 then
+        hold_str = "HOLD"
+    else
+        hold_str = "???"
+    end
+  -- draw timer time
+  lcd.drawText(wgt.zone.x, wgt.zone.y, hold_str, font_size + textColor)
+
+end
+
+return { name="ThrHold", options=options, create=create, update=update, background=background, refresh=refresh }
